d5f6dc503f412651cc81adfc13be76776f861645
Nathan Belza <nathanbelza1@gmail.com>
2026-06-04 19:52:38 -0400

    switch to esp_timer for sensor stream frequency control

--- main/sensor_stream.c
+++ main/sensor_stream.c
@@ -15,7 +15,7 @@
 #include "setup.h"
 #include "wifi_tools.h"
 
-#define SENSOR_READ_STACK_SIZE 4096
+#define SENSOR_READ_STACK_SIZE 2048
 #define MAX_FREQUENCY 250
 #define RING_BUFFER_LEN (UDP_SEND_QUEUE_LEN + 2)
 // +2 to account for packet taken out of queue by
@@ -84,6 +84,11 @@ static void s_read_sensor_task(void *pvParams) {
     }
 }
 
+static void s_stream_timer_callback(void *ctx) {
+    SemaphoreHandle_t *stream_timer_semaphore = (SemaphoreHandle_t *)ctx;
+    xSemaphoreGive(*stream_timer_semaphore);
+}
+
 void sensor_stream(void *pvParams) {
     app_ctx_t *app_ctx = (app_ctx_t *)pvParams;
 
@@ -94,19 +99,20 @@ void sensor_stream(void *pvParams) {
     // stack for static sensor read tasks
     static StaticTask_t sensor_read_task_buffer[CONFIG_NUM_SENSORS];
     static StackType_t sensor_read_stack[CONFIG_NUM_SENSORS][SENSOR_READ_STACK_SIZE];
-    static TaskHandle_t sensor_read_task_handle[CONFIG_NUM_SENSORS];
+    static TaskHandle_t sensor_read_task[CONFIG_NUM_SENSORS];
     // context for sensor read tasks
     static sensor_ctx_t sensor_ctx[CONFIG_NUM_SENSORS] = {0};
 
-    uint32_t data_idx = 0;
+    // start sensor read tasks
     for (size_t i = 0; i < CONFIG_NUM_SENSORS; i++) {
         sensor_ctx[i].sensor_read_done_semaphore = xSemaphoreCreateBinaryStatic(&sensor_read_done_semaphore_buffer[i]);
+        configASSERT(sensor_ctx[i].sensor_read_done_semaphore);
         sensor_ctx[i].sensor = &app_ctx->sensors[i];
 
         char task_name[25];
         snprintf(task_name, sizeof(task_name), "sensor_read_%u", i);
 
-        sensor_read_task_handle[i] = xTaskCreateStatic(
+        sensor_read_task[i] = xTaskCreateStatic(
             s_read_sensor_task,
             task_name,
             SENSOR_READ_STACK_SIZE,
@@ -116,12 +122,26 @@ void sensor_stream(void *pvParams) {
             &sensor_read_task_buffer[i]
         );
     }
+    uint32_t data_idx = 0; // initialize data ring buffer index to 0
 
-    uint32_t period_ms = 1000; // default to 1Hz
-    TickType_t xLastWakeTime = xTaskGetTickCount();
+    // set up stream timer for frequency control
+    StaticSemaphore_t stream_timer_semaphore_buffer;
+    SemaphoreHandle_t stream_timer_semaphore = xSemaphoreCreateBinaryStatic(&stream_timer_semaphore_buffer);
+    configASSERT(stream_timer_semaphore);
+
+    uint64_t period_us = 1000000ULL; // default to 1Hz
+
+    const esp_timer_create_args_t stream_timer_args = {
+        .callback = &s_stream_timer_callback,
+        .arg = &stream_timer_semaphore,
+        .name = "stream timer"
+    };
+    esp_timer_handle_t stream_timer;
+    esp_timer_create(&stream_timer_args, &stream_timer);
+    esp_timer_start_periodic(stream_timer, period_us);
 
     while (1) {
-
+        // wait for signal to start a reading
         xEventGroupWaitBits(
             app_ctx->sensor_stream_event_group_handle,
             SENSOR_STREAM_ENABLE_BIT | SENSORS_SINGLE_READING_BIT,
@@ -129,22 +149,30 @@ void sensor_stream(void *pvParams) {
             pdFALSE,
             portMAX_DELAY
         );
-        
-        qlcp_sensor_data *data = data_ring_buffer[data_idx];
-
+        // use task notification to recieve an updated frequency if available
         uint32_t new_frequency;
         if (xTaskNotifyWait(0, UINT32_MAX, &new_frequency, 0) == pdTRUE) {
             if (new_frequency > 0) {
                 const uint32_t limited_frequency = (new_frequency < MAX_FREQUENCY ? new_frequency : MAX_FREQUENCY);
-                period_ms = 1000 / limited_frequency;
+                period_us = 1000000ULL / limited_frequency;
                 ESP_LOGI(TAG, "Stream frequency: %u", limited_frequency);
-                xLastWakeTime = xTaskGetTickCount(); // update timestamp to avoid double sending packets on change
+                esp_timer_stop(stream_timer);
+                esp_timer_start_periodic(stream_timer, period_us);
             }
         }
+        // if single reading, clear bit to avoid relooping
+        if (xEventGroupGetBits(app_ctx->sensor_stream_event_group_handle) & SENSORS_SINGLE_READING_BIT) {
+            xEventGroupClearBits(app_ctx->sensor_stream_event_group_handle, SENSORS_SINGLE_READING_BIT);
+        } else {
+            xSemaphoreTake(stream_timer_semaphore, portMAX_DELAY);
+        }
+
+        // set pointer to current ring buffer position
+        qlcp_sensor_data *data = data_ring_buffer[data_idx];
 
         // start individual sensor read tasks
         for (size_t i = 0; i < CONFIG_NUM_SENSORS; i++) {
-            xTaskNotifyGive(sensor_read_task_handle[i]);
+            xTaskNotifyGive(sensor_read_task[i]);
         }
 
         // wait until all sensor reads are complete
@@ -160,30 +188,23 @@ void sensor_stream(void *pvParams) {
             }
         }
 
+        // create data packet
         const uint32_t current_ts_offset = atomic_load(&app_ctx->ts_offset);
         const uint32_t timestamp = current_ts_offset + (uint32_t)(esp_timer_get_time() / 1000);
         const uint16_t sequence = atomic_fetch_add(&app_ctx->sequence, 1);
 
         qlcp_data_packet data_packet = {
-            .sensor_data = data,
+            .sensor_data = data, // pointer to data buffer in ring buffer
             .sensor_count = CONFIG_NUM_SENSORS,
             .header = {
                        .sequence = sequence,
                        .timestamp = timestamp,
                        },
         };
-        // send data packets to the udp send queue
+        // send data packet to the udp send queue
         if (xQueueSend(app_ctx->network_ctx->udp_send_queue_handle, (void *)&data_packet, MESSAGE_QUEUE_TIMEOUT) == pdTRUE) {
+            // if packet added to queue increment ring buffer index
             data_idx = (data_idx + 1) % RING_BUFFER_LEN;
-        }
-        // if the queue is full and can't get another packet added, ring buffer index will be reused
-
-        // if single reading, clear bit to avoid relooping
-        if (xEventGroupGetBits(app_ctx->sensor_stream_event_group_handle) & SENSORS_SINGLE_READING_BIT) {
-            xEventGroupClearBits(app_ctx->sensor_stream_event_group_handle, SENSORS_SINGLE_READING_BIT);
-        } else {
-            // xTaskDelayUntil accounts for time taken to read sensors
-            xTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(period_ms));
-        }
+        } // else drop packet and reuse index
     }
 }
