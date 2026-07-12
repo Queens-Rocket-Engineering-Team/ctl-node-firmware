a6b2895f35e8f26dc07a84d711e5b71ece940e69
Nathan Belza <nathanbelza1@gmail.com>
2026-07-12 16:30:13 -0400

    update timestamps to us

--- main/sensor_stream.c
+++ main/sensor_stream.c
@@ -27,7 +27,6 @@ typedef struct {
     SemaphoreHandle_t sensor_read_done_semaphore;
     sensor_t *sensor;
     float value;
-    uint8_t unit;
 } sensor_ctx_t;
 
 static void s_read_sensor_task(void *pvParams) {
@@ -42,43 +41,6 @@ static void s_read_sensor_task(void *pvParams) {
             ctx->value = FLT_MAX; // assign max float value on fail
         }
 
-        // translate sensor unit to QLCP units
-        switch (ctx->sensor->base.unit) {
-        case SENSOR_UNIT_A:
-            ctx->unit = QLCP_UNIT_AMPS;
-            break;
-        case SENSOR_UNIT_N:
-            ctx->unit = QLCP_UNIT_NEWTONS;
-            break;
-        case SENSOR_UNIT_KG:
-            ctx->unit = QLCP_UNIT_KILOGRAMS;
-            break;
-        case SENSOR_UNIT_PSI:
-            ctx->unit = QLCP_UNIT_PSI;
-            break;
-        case SENSOR_UNIT_BAR:
-            ctx->unit = QLCP_UNIT_BAR;
-            break;
-        case SENSOR_UNIT_PA:
-            ctx->unit = QLCP_UNIT_PASCAL;
-            break;
-        case SENSOR_UNIT_OHMS:
-            ctx->unit = QLCP_UNIT_OHMS;
-            break;
-        case SENSOR_UNIT_C:
-            ctx->unit = QLCP_UNIT_CELSIUS;
-            break;
-        case SENSOR_UNIT_K:
-            ctx->unit = QLCP_UNIT_KELVIN;
-            break;
-        case SENSOR_UNIT_F:
-            ctx->unit = QLCP_UNIT_FAHRENHEIT;
-            break;
-        case SENSOR_UNIT_UNITLESS:
-        default:
-            ctx->unit = QLCP_UNIT_UNITLESS;
-            break;
-        }
         // give back semaphore on completion
         xSemaphoreGive(ctx->sensor_read_done_semaphore);
     }
@@ -180,25 +142,23 @@ void sensor_stream(void *pvParams) {
             if (xSemaphoreTake(sensor_ctx[i].sensor_read_done_semaphore, pdMS_TO_TICKS(250)) == pdTRUE) {
                 data[i].sensor_id = i;
                 data[i].value = sensor_ctx[i].value;
-                data[i].unit = sensor_ctx[i].unit;
             } else {
                 data[i].sensor_id = i;
                 data[i].value = FLT_MAX;
-                data[i].unit = QLCP_UNIT_UNITLESS;
             }
         }
 
         // create data packet
-        const uint32_t current_ts_offset = atomic_load(&app_ctx->ts_offset);
-        const uint32_t timestamp = current_ts_offset + (uint32_t)(esp_timer_get_time() / 1000);
-        const uint16_t sequence = atomic_fetch_add(&app_ctx->sequence, 1);
+        const uint64_t current_ts_offset = atomic_load(&app_ctx->ts_offset);
+        const uint64_t timestamp_us = current_ts_offset + esp_timer_get_time();
+        const uint8_t sequence = atomic_fetch_add(&app_ctx->sequence, 1);
 
         qlcp_data_packet data_packet = {
             .sensor_data = data, // pointer to data buffer in ring buffer
             .sensor_count = CONFIG_NUM_SENSORS,
             .header = {
                        .sequence = sequence,
-                       .timestamp = timestamp,
+                       .timestamp_us = timestamp_us,
                        },
         };
         // send data packet to the udp send queue
--- main/setup.h
+++ main/setup.h
@@ -17,8 +17,8 @@ typedef struct {
     network_ctx_t *network_ctx;
     EventGroupHandle_t sensor_stream_event_group_handle;
     TaskHandle_t sensor_stream_handle;
-    atomic_int_least32_t ts_offset;
-    atomic_uint_least16_t sequence;
+    atomic_uint_least64_t ts_offset;
+    atomic_uint_least8_t sequence;
     i2c_master_bus_handle_t bus_handle;
 } app_ctx_t;
 
